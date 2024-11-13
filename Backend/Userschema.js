8d96dd3f08b18110cbe9013790016e35ae0f0273
Tanishq <tanishqmishra9936@gmail.com>
2024-11-13 16:14:50 +0530

    Pushed on 13 November

---
+++ Backend/Userschema.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+  name:{
+    type:String,
+    required:true
+  },
+  mailid:{
+    type:String,
+    required:true
+  },
+  mobileno:{
+    type:Number,
+    required:true
+  },
+  password:{
+    type:String,
+    required:true
+  },
+  Date:{
+    type:String,
+    required:true
+  }
+});
+
+const User = model('users', userSchema);
+module.exports = mongoose.models.users || User
---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const mongoose =require('mongoose')
+const User = require('./Backend/Userschema')
+mongoose.connect('mongodb://127.0.0.1:27017/estate_explorer')
+app.get('/',async (req, res) => {
+  res.send("Hello")
+})
+app.post('/adduser',async (req,res)=>{
+
+    // const AddUser = new User({
+    //     name:req.body.name,
+    //     mailid:req.body.email,
+    //     mobileno:req.body.mobile,
+    //     password:req.body.password
+    //   })
+    //   await AddUser.save()
+    console.log(req)
+      res.send("Data Saved Success")
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
