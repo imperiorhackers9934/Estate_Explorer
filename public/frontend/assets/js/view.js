0a6e02c6d7a3f4389f5462ac71201bc79ee076c4
Imperior Hackers <58589595+imperiorhackers9934@users.noreply.github.com>
2025-01-07 10:38:35 +0530

    Update view.js

--- public/frontend/assets/js/view.js
+++ public/frontend/assets/js/view.js
@@ -11,7 +11,7 @@ async function fetchPropertyData() {
       }
   
       // Define the API endpoint
-      const apiUrl = `https://${window.location.hostname}/${userId}`;
+      const apiUrl = `/properties/${userId}`;
       
       // Make a POST request to fetch property data
       const response = await fetch(apiUrl, {
@@ -28,7 +28,7 @@ async function fetchPropertyData() {
         
         const data = await response.json();
         console.log('Property data:', data);
-        const usrUrl = `https://${window.location.hostname}/getname/${data.userId}`;
+        const usrUrl = `/getname/${data.userId}`;
         document.title=data.address
         // Make a POST request to fetch property data
       const myres = await fetch(usrUrl, {
