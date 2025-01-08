519bfc6bbd7decbc5f3046a7111d56c01bed923d
Imperior Hackers <58589595+imperiorhackers9934@users.noreply.github.com>
2025-01-08 11:35:27 +0530

    Update dash.js

--- public/frontend/assets/js/dash.js
+++ public/frontend/assets/js/dash.js
@@ -1,9 +1,9 @@
 let gettoken=localStorage.getItem("auth-token")
 if(!gettoken){
     alert("Session Expired")
-    window.location="../../login.html"
+    window.location="../../Login.html"
 }
 document.getElementById("logout").addEventListener("click",function(){
     localStorage.removeItem("auth-token")
-    window.location="../../login.html"
+    window.location="../../Login.html"
 })
